c22c3d0e1aba8b17a946f5c4bf2e68ffc374dde8
Vale Restan <valeresmune10@gmail.com>
2026-03-05 13:21:26 -0500

    Avansez

---
+++ Ejercicios/lectura.js
@@ -0,0 +1,4 @@
+const entrada = require("prompt-sync")();
+
+let nombre = entrada("Ingrese su nombre: ");
+console.log("su nombre es " + nombre);
---
+++ Ejercicios/variables.js
@@ -0,0 +1,7 @@
+//programa para calcular el area de un circulo
+//area = pi * r^2
+const radio = 6;
+let area = Math.PI * Math.pow(radio, 2);
+
+let cabecara = "El area del circulo es: " + area;
+console.log(cabecara);
